b1cd8bd85693c0381497ac6c6b0ebbc741c2c96b
张正亮 <zhangzhliang@yonyou.com>
2026-06-25 13:40:35 +0800

    feat: add score statistics starter exercise

---
+++ exercises/java-basics/starter/src/main/java/io/github/javamastery/exercises/basics/statistics/ScoreReport.java
@@ -0,0 +1,11 @@
+package io.github.javamastery.exercises.basics.statistics;
+
+public final class ScoreReport {
+
+    private ScoreReport() {
+    }
+
+    public static String build(String scoreText, int passScore) {
+        return "";
+    }
+}
